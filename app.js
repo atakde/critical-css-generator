b8eb2011ab2ac76dba23a2f57f8fc477a6ae273a
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2021-11-23 23:35:29 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const penthouse = require("penthouse");
+const path = require("path");
+const getCss = require("get-css");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/static/index.html"));
+});
+
+app.post("/generate", (req, res) => {
+  let url = req.body.url;
+  const options = {
+    timeout: 5000,
+  };
+  getCss(url, options)
+    .then(function (response) {
+      penthouse({
+        url: url,
+        cssString: response.css,
+      }).then((criticalCss) => {
+        res.send(criticalCss);
+      });
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
